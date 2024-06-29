2fd8e49e87c9564fc9c2fb55d965bc1fe5afb184
ZoodiacR <andyleon000820@gmail.com>
2024-06-29 13:10:55 -0400

    Update urls.py

--- Task/Task/urls.py
+++ Task/Task/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tareas.views import TaskViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
