fbd5ce8709498f960636ca58a49105a8c0c9def5
ZoodiacR <andyleon000820@gmail.com>
2024-06-29 11:49:50 -0400

    Update urls.py

--- Task/Task/urls.py
+++ Task/Task/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('', include(router.urls)),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
